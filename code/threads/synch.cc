276171a65a5162413bf37a4be0e4c4360da6793f
Chintan <cvora@westhealth.org>
2013-02-28 15:31:01 -0800

    Testing Debug Statements

--- code/threads/synch.cc
+++ code/threads/synch.cc
@@ -163,6 +163,9 @@ void Lock::Acquire()
     }
 
     (void) interrupt->SetLevel(oldLevel);
+
+    DEBUG('t', "Lock Acquire DONE !!!! \n");
+
 }
 
 //----------------------------------------------------------------------
