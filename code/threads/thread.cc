2207cc38faf15ce974b62516a7db195b1bb91ba4
Chintan <cvora@westhealth.org>
2013-03-01 16:00:05 -0800

    Yield Print Fixed

--- code/threads/thread.cc
+++ code/threads/thread.cc
@@ -179,7 +179,7 @@ Thread::Yield ()
     
     ASSERT(this == currentThread);
     
-    DEBUG('t', "Yielding thread \"%s\"\n", getName());
+    DEBUG('t', "\nYielding thread \"%s\"\n", getName());
     
     nextThread = scheduler->FindNextToRun();
     if (nextThread != NULL) {
