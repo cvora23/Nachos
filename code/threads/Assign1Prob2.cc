977d3cf55e597a19af245293a0680be6d6cf3f6c
Chintan <cvora@westhealth.org>
2013-03-03 22:45:30 -0800

    Initialization of Customer Thread with init info almost done !!!!

--- code/threads/Assign1Prob2.cc
+++ code/threads/Assign1Prob2.cc
@@ -7,27 +7,94 @@
 
 #include "Assign1Prob2.h"
 
+/**
+ * GLOBAL DATA STRUCTURES
+ */
+ItemInfo g_ItemInfo[MAX_NO_OF_ITEMS];
+
+CustomerInfo g_CustomerInfo[NO_OF_CUSTOMERS];
+
+/**
+ * GLOBAL DATA STRUCTURES INIT FUNCTIONS
+ */
+void initItemInfo()
+{
+	for (int i = 0;i<MAX_NO_OF_ITEMS;i++)
+	{
+		g_ItemInfo[i].Price = Random()%MAX_PRICE_PER_ITEM + 1;
+		g_ItemInfo[i].departmentNo = Random()%NO_OF_DEPARTMENT;
+		g_ItemInfo[i].shelfNo = Random()%NO_OF_SHELFS;
+	}
+}
+
+void initCustomerInfo()
+{
+	for(int i = 0;i<NO_OF_CUSTOMERS;i++)
+	{
+		g_CustomerInfo[i].money = Random()%MAX_AMT_PER_CUSTOMER + 1;
+		g_CustomerInfo[i].type = Random()%NON_PRIVILEGED + 1;
+		g_CustomerInfo[i].noOfItems = Random()%MAX_NO_ITEMS_TO_BE_PURCHASED + 1;
+		g_CustomerInfo[i].pCustomerShoppingList = new CustomerShoppingList[g_CustomerInfo[i].noOfItems];
+	}
+}
+
+void initCustomerShoppingList()
+{
+	for(int i = 0;i<NO_OF_CUSTOMERS;i++)
+	{
+		for(int j =0;j<g_CustomerInfo[i].noOfItems;j++)
+		{
+			g_CustomerInfo[i].pCustomerShoppingList[j].itemNo = Random()%NO_OF_ITEM_TYPES;
+			g_CustomerInfo[i].pCustomerShoppingList[j].noOfItems =
+					Random()%MAX_NO_ITEMS_TO_BE_PURCHASED_OF_EACH_TYPE + 1;
+		}
+	}
+}
+
+void printCustomerInfo(int customerId)
+{
+	DEBUG('p',"Customer ID is \n",
+			customerId);
+	DEBUG('p',"Customer %d is of type %d \n",
+			customerId,g_CustomerInfo[customerId].type);
+	DEBUG('p',"Customer %d can spend %d amount on shopping \n",
+			customerId,g_CustomerInfo[customerId].money);
+	DEBUG('p',"Customer %d will purchase %d items today for shopping \n",
+			customerId,g_CustomerInfo[customerId].noOfItems);
+}
+
+void printCustomerShoppingInfo(int customerId)
+{
+	DEBUG('p',"Customer %d shopping list is as follows : ",customerId);
+	for(int j =0;j<g_CustomerInfo[customerId].noOfItems;j++)
+	{
+		DEBUG('p',"Item No: %d\n",
+				g_CustomerInfo[j].pCustomerShoppingList[j].itemNo);
+		DEBUG('p',"No of Items of Item Type:%d ===== %d\n",
+				g_CustomerInfo[j].pCustomerShoppingList[j].itemNo,
+				g_CustomerInfo[j].pCustomerShoppingList[j].noOfItems);
+	}
+}
+
 void CustomerThread(int ThreadId)
 {
     DEBUG('p', "%s Started !!!!!!! \n",currentThread->getName());
+    printCustomerInfo(ThreadId);
 }
 
 void GoodLoaderThread(int ThreadId)
 {
     DEBUG('p', "%s Started !!!!!!! \n",currentThread->getName());
-
 }
 
 void SalesmanThread(int ThreadId)
 {
     DEBUG('p', "%s Started !!!!!!! \n",currentThread->getName());
-
 }
 
 void CashierThread(int ThreadId)
 {
     DEBUG('p', "%s Started !!!!!!! \n",currentThread->getName());
-
 }
 
 void ManagerThread(int ThreadId)
@@ -35,13 +102,20 @@ void ManagerThread(int ThreadId)
     DEBUG('p', "%s Started !!!!!!! \n",currentThread->getName());
 }
 
-void Problem2()
+void startSimulation()
 {
 	Thread* t;
+	RandomInit(NULL);
+
     DEBUG('p', "Entering Assign 1 Problem 2 !!!!!!! \n");
     DEBUG('p', "Starting to initialize all the Threads for Problem 2 !!!!!!! \n");
 
-    DEBUG('p', "Starting Manager for Problem 2 !!!!!!! \n");
+    DEBUG('p',"Number of Cashiers = %d \n",NO_OF_CASHIERS);
+    DEBUG('p',"Number of Good Loaders = %d \n",NO_OF_GOOD_LOADERS);
+    DEBUG('p',"Number of PrivilegedCustomers = %d \n",NO_OF_CUSTOMERS);
+    DEBUG('p',"Number of Customers = %d \n",NO_OF_CUSTOMERS);
+    DEBUG('p',"Number of Managers = %d \n",NO_OF_MANAGERS);
+    DEBUG('p',"Number of DepartmentSalesmen = %d \n",NO_OF_SALESMAN);
 
     char *threadName;
 
@@ -80,4 +154,9 @@ void Problem2()
     	t = new Thread(threadName);
     	t->Fork((VoidFunctionPtr)CustomerThread,i);
     }
+}
+
+void Problem2()
+{
+	startSimulation();
 }
--- code/threads/Assign1Prob2.h
+++ code/threads/Assign1Prob2.h
@@ -10,21 +10,84 @@
 
 #include "system.h"
 
-#define NO_OF_MANAGERS				1
-#define NO_OF_CASHIERS				5
-#define NO_OF_DEPARTMENT			5
-#define NO_OF_SALESMAN				(NO_OF_DEPARTMENT*3)
-#define NO_OF_GOOD_LOADERS			5
-#define NO_OF_CUSTOMERS				30
+/**
+ * STORE STOCK INFO
+ */
+#define NO_OF_DEPARTMENT								5
+#define NO_OF_SHELFS									(NO_OF_DEPARTMENT*3)
+#define NO_OF_ITEM_TYPES								NO_OF_SHELFS
+#define MAX_NO_ITEMS_TO_BE_PURCHASED					10
+#define MAX_NO_ITEMS_TO_BE_PURCHASED_OF_EACH_TYPE 		5
+#define MAX_AMT_PER_CUSTOMER							1000
+#define MAX_PRICE_PER_ITEM								200
+#define MAX_NO_OF_ITEMS									5000
 
-#define space_conc(str1, str2) #str1 " " #str2
+/**
+ * STORE MANAGEMENT INFO
+ */
+#define NO_OF_MANAGERS									1
+#define NO_OF_CASHIERS									5
+#define NO_OF_SALESMAN									(NO_OF_DEPARTMENT*3)
+#define NO_OF_GOOD_LOADERS								5
+#define NO_OF_CUSTOMERS									30
 
-#define MANAGER_STRING				(const char*)"MANAGER"
-#define CASHIER_STRING				(const char*)"CASHIER"
-#define DEPARTMENT_STRING			(const char*)"DEPARTMENT"
-#define SALESMAN_STRING				(const char*)"SALESMAN"
-#define GOOD_LOADERS_STRING			(const char*)"GOOD LOADER"
-#define CUSTOMER_STRING				(const char*)"CUSTOMER"
+#define MANAGER_STRING									(const char*)"MANAGER"
+#define CASHIER_STRING									(const char*)"CASHIER"
+#define DEPARTMENT_STRING								(const char*)"DEPARTMENT"
+#define SALESMAN_STRING									(const char*)"SALESMAN"
+#define GOOD_LOADERS_STRING								(const char*)"GOOD LOADER"
+#define CUSTOMER_STRING									(const char*)"CUSTOMER"
 
+typedef struct _ItemInfo
+{
+	/**
+	 * Price of the item
+	 */
+	int 	Price;
+	/**
+	 * Shelf where the item is placed
+	 */
+	int 	shelfNo;
+	/**
+	 * Department No where item is from
+	 */
+	int 	departmentNo;
+}ItemInfo;
+
+static void initItemInfo();
+
+typedef struct _CustomerShoppingList
+{
+	int 	 itemNo;
+	int		 noOfItems;
+}CustomerShoppingList;
+
+typedef enum _CustomerType{PRIVILEGED = 0,NON_PRIVILEGED}CustomerType;
+
+typedef struct _CustomerInfo
+{
+	/**
+	 * Type of Customer - Privileged or Non Privileged
+	 */
+	CustomerType 			type;
+	/**
+	 * Money Customer can spend for shopping
+	 */
+	int 					money;
+	/**
+	 * Shopping List Item Count
+	 */
+	int						noOfItems;
+	/**
+	 * Shopping List for Customer
+	 */
+	CustomerShoppingList	*pCustomerShoppingList;
+}CustomerInfo;
+
+static void initCustomerInfo();
+static void initCustomerShoppingList();
+void printCustomerInfo(int customerId);
+
+void startSimulation();
 
 #endif /* ASSIGN1PROB2_H_ */
