2f5d49fa641f046b1c14de4d2656a951d313dd3c
Chintan <cvora@westhealth.org>
2013-03-03 23:19:40 -0800

    Minor Fix

--- code/threads/Assign1Prob2.cc
+++ code/threads/Assign1Prob2.cc
@@ -36,7 +36,7 @@ static void initCustomerInfo()
 		for(int i = 0;i<NO_OF_CUSTOMERS;i++)
 		{
 			g_CustomerInfo[i].money = Random()%MAX_AMT_PER_CUSTOMER + 1;
-			g_CustomerInfo[i].type = CustomerType(Random()%NON_PRIVILEGED + 0);
+			g_CustomerInfo[i].type = CustomerType(Random()%2);
 			g_CustomerInfo[i].noOfItems = Random()%MAX_NO_ITEMS_TO_BE_PURCHASED + 1;
 			g_CustomerInfo[i].pCustomerShoppingList = new CustomerShoppingList[g_CustomerInfo[i].noOfItems];
 		}
