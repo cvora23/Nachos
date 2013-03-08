212357463a3369b846551b1272f3699fec0a5ffc
Chintan <cvora@westhealth.org>
2013-03-08 15:22:30 -0800

    test2

--- code/threads/test2.sh
+++ code/threads/test2.sh
@@ -1,10 +1,12 @@
 #!/bin/tcsh
 
 usb_path="/media/disk/"
+echo $usb_path
 
-if[ -f $usb_path ]
-then
-    echo "path exists"
-else
-    echo "path does not exist"
-fi
+
+#if[ -f $usb_path ]
+#then
+#    echo "path exists"
+#else
+#    echo "path does not exist"
+#fi
