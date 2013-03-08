21dbe1c20e17477c07338751172660d39b1c7b38
Chintan <cvora@westhealth.org>
2013-03-08 13:53:32 -0800

    test

--- code/threads/test2.sh
+++ code/threads/test2.sh
@@ -1,6 +1,6 @@
 #!/bin/tcsh
 
-$DIRECTORY=/home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs
+DIRECTORY=/home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs
 if if [! -d "$DIRECTORY" ]
   then
     echo "ABSENT"
