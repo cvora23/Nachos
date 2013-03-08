bcd7ae827d7abf828629bff86a28de55598fb547
Chintan <cvora@westhealth.org>
2013-03-08 15:35:47 -0800

    test

--- code/threads/test1.sh
+++ code/threads/test1.sh
@@ -2,17 +2,21 @@
 
 set DIRECTORY = "/home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs"
 
-    if [ ! -d "$DIRECTORY" ]; 
-    then
-            mkdir $DIRECTORY
-    else
-            script -f -q $DIRECTORY/$$.log
-    endif
-
-gdb nachos <<GDB_INPUT
-pwd
-run nachos -P2 -d p
-quit
-   exit
-
-GDB_INPUT
+    if [ -d $DIRECTORY ]; 
+then
+    echo "path exists"
+else
+    echo "path does not exist"
+fi
+#            mkdir $DIRECTORY
+#    else
+#            script -f -q $DIRECTORY/$$.log
+#    endif
+#
+#gdb nachos <<GDB_INPUT
+#pwd
+#run nachos -P2 -d p
+#quit
+#   exit
+#
+#GDB_INPUT
