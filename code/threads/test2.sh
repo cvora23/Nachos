f85c9a271b9405fc4a39881a55b0e46eee367b21
Chintan <cvora@westhealth.org>
2013-03-08 13:57:44 -0800

    test

--- code/threads/test2.sh
+++ code/threads/test2.sh
@@ -1,9 +1,9 @@
-#!/bin/tcsh
+s#!/bin/tcsh
 
-DIRECTORY="/home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs"
-if if [! -d "$DIRECTORY" ]
-  then
-    echo "ABSENT"
+dir="/home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs"
+
+if [ "$(ls -A $dir)" ]; then
+  echo "$dir not empty"
 else
-    echo "PRESENT"
+  echo "$dir is empty"
 fi
