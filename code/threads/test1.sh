13da29e30c2e7186ff6c68c9c35a8469a84b70f3
Chintan <cvora@westhealth.org>
2013-03-08 13:32:32 -0800

    test1 script

--- code/threads/test1.sh
+++ code/threads/test1.sh
@@ -1,18 +1,18 @@
 #!/bin/tcsh
 
-    if [ ! -d /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs ]
-     then
+    if [ ! -d /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs ] then
+     
         mkdir /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs
         else
     script -f -q /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs/$$.log
-
+    endif
 
 gdb nachos <<GDB_INPUT
 pwd
 run nachos -P2 -d p
 quit
    exit
-   fi
+   
 
 
 GDB_INPUT
