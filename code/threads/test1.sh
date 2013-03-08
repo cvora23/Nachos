f4418203299a8a9270c8c3d0c9d6e302b47ae904
Chintan <cvora@westhealth.org>
2013-03-08 15:49:10 -0800

    test

--- code/threads/test1.sh
+++ code/threads/test1.sh
@@ -3,19 +3,13 @@
 set DIRECTORY = /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs
 
     if ( ! -d "$DIRECTORY" ) then
-    echo "Directory does not exist.. So making a new directory $DIRECTORY"
-    else
-    echo "Directory does exist"
-    endif
-#            mkdir $DIRECTORY
-#    else
-#            script -f -q $DIRECTORY/$$.log
-#    endif
-#
-#gdb nachos <<GDB_INPUT
-#pwd
-#run nachos -P2 -d p
-#quit
-#   exit
-#
-#GDB_INPUT
+        echo "Directory does not exist.. So making a new directory $DIRECTORY"
+        mkdir $DIRECTORY
+    fi
+    script -f -q $DIRECTORY/$$.log
+    gdb nachos <<GDB_INPUT
+    pwd
+    run nachos -P2 -d p
+    quit
+    exit
+    GDB_INPUT
