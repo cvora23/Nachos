a93436792ab72771e49e83f6b27b1201c5d91efd
Chintan <cvora@westhealth.org>
2013-03-08 13:13:11 -0800

    TEST SCRIPT

--- code/threads/Testing.sh
+++ code/threads/Testing.sh
@@ -1,12 +1,9 @@
 #!/bin/tcsh
 
-if [ -z "$UNDER_SCRIPT" ]; then
     if [ ! -d /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs ]; then
         mkdir /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs
     fi
-    export UNDER_SCRIPT=1
     script -f -q /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs/$$.log
-
 
 gdb nachos <<GDB_INPUT
 pwd
