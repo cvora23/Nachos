458795061f6d2f99ff4aa0bdfee9479436cd7b15
Chintan <cvora@westhealth.org>
2013-03-08 13:50:49 -0800

    TES2 SCRIPT

--- code/threads/test2.sh
+++ code/threads/test2.sh
@@ -1,7 +1,9 @@
 #!/bin/tcsh
 
-if if [ ! -d "/home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs" ];  then
-    echo "present"
+DIRECTORY="/home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs"
+if if [! -d "$DIRECTORY" ]
+  then
+    echo "ABSENT"
     fi
 else
     echo "absent"
