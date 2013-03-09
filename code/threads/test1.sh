4855610505f9c547408213325fd5d5703b8d49a7
Chintan <cvora@westhealth.org>
2013-03-08 16:18:37 -0800

    test1 script

--- code/threads/test1.sh
+++ code/threads/test1.sh
@@ -6,10 +6,11 @@ set DIRECTORY = /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs
         echo "Directory does not exist.. So making a new directory $DIRECTORY"
         mkdir $DIRECTORY
     endif
-    script /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs/$$.log
- #   gdb nachos <<GDB_INPUT
- #   pwd
- #   run nachos -P2 -d p
- #   quit
- #   exit
- #   GDB_INPUT
+    
+      script /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs/$$.log
+      gdb nachos <<GDB_INPUT
+      pwd
+      run nachos -P2 -d p
+      quit
+      exit
+      GDB_INPUT
