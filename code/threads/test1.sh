cb2be9df00a55d2a019083174129f70cb44597fc
Chintan <cvora@westhealth.org>
2013-03-08 16:12:13 -0800

    test1 script

--- code/threads/test1.sh
+++ code/threads/test1.sh
@@ -7,9 +7,9 @@ set DIRECTORY = /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs
         mkdir $DIRECTORY
     endif
     script -f -q /home/scf-05/ptaskar/nachos-csci402/code/threads/terminal-logs/$$.log
-    gdb nachos <<GDB_INPUT
-    pwd
-    run nachos -P2 -d p
-    quit
-    exit
-    GDB_INPUT
+ #   gdb nachos <<GDB_INPUT
+ #   pwd
+ #   run nachos -P2 -d p
+ #   quit
+ #   exit
+ #   GDB_INPUT
