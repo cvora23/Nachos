549a9365e3fb3b6ab03d32aa04e22e06201e639a
Chintan <cvora@westhealth.org>
2013-03-19 23:08:32 -0700

    test script nacos

--- code/threads/testscript.sh
+++ code/threads/testscript.sh
@@ -4,10 +4,15 @@ set arg1 = $1
 set arg2 = $2
 
 #GETTING ALL THE COMMAND LINE ARGUMENTS
-#set nachosOption="$1"
-#set testFile="$2"
-#set enableDebugging="$3"
-#set debugOptions="$4"
+set nachosOption = $1
+set testFile = $2
+set enableDebugging = $3
+set debugOptions = $4
+
+echo "$nachosOption"
+echo "$testFile"
+echo "$enableDebugging"
+echo "$debugOptions"
 
 #CREATING A NEW GDB SESSION LOG FILE
 #logFile=$testFile.$(date +"%F_%T").log
