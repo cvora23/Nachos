be944d412bcffef0a68a5a80eb142fbe750c497d
Chintan <cvora@westhealth.org>
2013-03-19 23:59:59 -0700

    test script nacos

--- code/threads/testscript.sh
+++ code/threads/testscript.sh
@@ -5,11 +5,13 @@ set nachosOption = $1
 set testFile = $2
 set enableDebugging = $3
 set debugOptions = $4
+set currentDateTime = 'date +”%y%m%d”'
 
 echo "$nachosOption"
 echo "$testFile"
 echo "$enableDebugging"
 echo "$debugOptions"
+echo "$currentDateTime"
 
 #CREATING A NEW GDB SESSION LOG FILE
 #set logFile = "$testFile"."$(date +"%F_%T")".log
