d83445fbfd8faeb317e97c6ee2138d6a3a8bd19e
Chintan <cvora@westhealth.org>
2013-03-20 00:15:38 -0700

    test script nacos

--- code/threads/testscript.sh
+++ code/threads/testscript.sh
@@ -7,13 +7,13 @@ set enableDebugging = $3
 set debugOptions = $4
 set currentDateTime = `date +'%d-%m-%Y_%H-%M-%S'`
 
-: << --COMMENT BLOCK--
+: << --END-COMMENT--
 echo "$nachosOption"
 echo "$testFile"
 echo "$enableDebugging"
 echo "$debugOptions"
 echo "$currentDateTime"
---COMMENTS BLOCK--
+--END-COMMENT--
 
 #CREATING A NEW GDB SESSION LOG FILE
 #set logFile = "$testFile"."$(date +"%F_%T")".log
