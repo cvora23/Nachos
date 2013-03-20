e901701f954944ce3163ef647b206aba82907ac0
Chintan <cvora@westhealth.org>
2013-03-19 23:05:41 -0700

    test script nacos

--- code/threads/testscript.sh
+++ code/threads/testscript.sh
@@ -1,18 +1,21 @@
 #!/bin/tcsh
 
+set arg1 = $1
+set arg2 = $2
+
 #GETTING ALL THE COMMAND LINE ARGUMENTS
-set nachosOption="$1"
-set testFile="$2"
-set enableDebugging="$3"
-set debugOptions="$4"
+#set nachosOption="$1"
+#set testFile="$2"
+#set enableDebugging="$3"
+#set debugOptions="$4"
 
 #CREATING A NEW GDB SESSION LOG FILE
-logFile=$testFile.$(date +"%F_%T").log
+#logFile=$testFile.$(date +"%F_%T").log
 
 #STARTING GDB SESSION
-gdb nachos <<GDB_INPUT
-pwd
-run nachos -$nachosOption $testFile $enableDebugging $debugOptions >$logFile
+#gdb nachos <<GDB_INPUT
+#pwd
+#run nachos -$nachosOption $testFile $enableDebugging $debugOptions >$logFile
 #ENDING GDB SESSION 
-quit
-GDB_INPUT
+#quit
+#GDB_INPUT
