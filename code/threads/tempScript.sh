2840157a0dda899a2b9361099ac27f48d516f110
Chintan <cvora@westhealth.org>
2013-03-21 22:12:52 -0700

    temp script

---
+++ code/threads/tempScript.sh
@@ -0,0 +1,11 @@
+#!/bin/tcsh
+
+set number1=5
+set number2=10
+
+
+if( $number1 >= $number2 && $number1 <= $number2 ) then
+    echo "number1 is is equal to number2"
+else
+    echo "number1 is not equal to number2"
+endif    
