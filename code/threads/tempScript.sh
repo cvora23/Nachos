5692762f778fc1876c589c7b7fde1f1d9dcda824
Chintan <cvora@westhealth.org>
2013-03-21 22:16:56 -0700

    temp

--- code/threads/tempScript.sh
+++ code/threads/tempScript.sh
@@ -4,7 +4,7 @@ set number1=5
 set number2=10
 
 
-if ($number1 >= $number2 && $number1 <= $number2) then
+if ($number1 > $number2) then
     echo "number1 is is equal to number2"
 else
     echo "number1 is not equal to number2"
