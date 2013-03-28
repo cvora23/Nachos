4a43c0e42d31319812741a94a8a33778ab97ca7a
Chintan <cvora@westhealth.org>
2013-03-28 15:44:21 -0700

    sprintf printString System call

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -731,7 +731,7 @@ void PrintString_Syscall(unsigned int vaddr,unsigned int string)
 	}
 
 	char tempStringBuf[MAX_CHAR_PRINTF];
-	if(copyin(vaddr,MAX_CHAR_PRINTF - 1,tempStringBuf) == -1)
+	if(copyin(string,MAX_CHAR_PRINTF - 1,tempStringBuf) == -1)
 	{
 		printf("%s: Bad Virtual Address \n",currentThread->getName());
 		return ;
