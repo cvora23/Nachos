a2130e9f9fb7fb487decc0c6be9530aada1871db
Chintan <cvora@westhealth.org>
2013-04-01 14:40:40 -0700

    trying to fix a memory corruption problem... Main memory of nachos

--- code/test/WalmartSimulation.c
+++ code/test/WalmartSimulation.c
@@ -1887,14 +1887,11 @@ void initLockCvForSimulation1()
     	print2("customerCashierLock %d = %d \n",i,g_customerCashierLock[i]);
 
 
-
-
+        g_managerCashierCashLock[i] = createLock("managerCashierCashLock",sizeof("managerCashierCashLock"));
+    	print2("managerCashierCashLock %d = %d \n",i,g_managerCashierCashLock[i]);
 
         g_customerCashierCV[i] = createCondition("customerCashierCV",sizeof("customerCashierCV"));
     	print2("customerCashierCV %d = %d \n",i,g_customerCashierCV[i]);
-
-        g_managerCashierCashLock[i] = createLock("managerCashierCashLock",sizeof("managerCashierCashLock"));
-    	print2("managerCashierCashLock %d = %d \n",i,g_managerCashierCashLock[i]);
 
         g_cashierWaitQueue[i] = 0;
 
